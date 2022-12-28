c0847457e3feb9f4e6c6617e192ae55794ae94c0
Kushagra Anit Singh <105034224+kushagra-a-singh@users.noreply.github.com>
2022-12-29 00:02:08 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+app.post("/", function (req, res) {
+  var n1 = Number(req.body.n1);
+  var n2 = Number(req.body.n2);
+  var result = n1 + n2;
+  res.send("The result of your calculation is : " + result);
+});
+
+app.listen(8000, () => console.log("Server is running on port 8000."));
